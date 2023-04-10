3ffa84ff21dad279b87757c76a18b4a328c5cd4c
sobbaka <a.bagaev1989@gmail.com>
2023-04-10 10:21:48 +0400

    add methods to show/hide cart icon

--- js/cart/view.js
+++ js/cart/view.js
@@ -2,6 +2,7 @@ export default class View {
   elements = {
     cartLink: document.querySelector(".cart__link"),
     cartModal: document.querySelector(".modal-cart"),
+    productsWrapper: document.querySelector("#products-wrapper"),
   }
 
   showCartModal() {
@@ -11,4 +12,13 @@ export default class View {
   hideCartModal() {
     this.elements.cartModal.classList.add("hidden");
   }
+
+  showCartIcon() {
+    this.elements.cartLink.classList.remove("hidden");
+  }
+
+  hideCartIcon() {
+    this.elements.cartLink.classList.add("hidden");
+  }
+
 }
