429f50b753a5cdabad966bae4edf1d2275a9f671
sobbaka <a.bagaev1989@gmail.com>
2023-04-15 23:30:56 +0400

    fix total price calculation

--- js/cart/model.js
+++ js/cart/model.js
@@ -28,7 +28,7 @@ export default class Model {
 
   getTotalPrice() {
     const total = this.cart.reduce((total, product) => {
-      return total + product.price;
+      return total + product.price * product.counter;
     }, 0)
     return total;
   }
@@ -42,5 +42,42 @@ export default class Model {
 
   getProduct(id) {
     return this.cart.find((item) => item.id === id);
+  }
+
+  updateCounter(id, action) {
+    const product = this.getProduct(id);
+
+    if (action === "plus") {
+      ++product.counter;
+    }
+
+    if (action === "minus" && product.counter > 0) {
+      --product.counter;
+    }
+
+    if (product.counter === 0) this.removeFromCart(id);
+
+    this.saveCartToLocalStorage();
+    return product;
+  }
+
+  removeFromCart(id) {
+    const index = this.cart.findIndex(item => item.id === id);
+    if (index !== -1) {
+      arr.splice(index, 1);
+    }
+  }
+
+
+  increaseCounter(id) {
+    const product = this.getProduct(id);
+
+  }
+
+  descreaseCounter(id) {
+    const product = this.getProduct(id);
+    product.counter -= 1;
+
+    if (product.counter === 0) { }
   }
 }
