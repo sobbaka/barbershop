602ee61e98f1f1c9761261970a5c2617574b5a40
sobbaka <a.bagaev1989@gmail.com>
2023-04-19 19:37:22 +0400

    refactor burger menu script

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-// window.onload = function () {
-//   const menu = document.querySelector('.navigation')
-//   const btn = document.querySelector('.hamburger')
-
-//   btn.onclick = () => {
-//     menu.classList.toggle('is-active')
-//     btn.classList.toggle('is-active')
-//   }
-// }
-
-
 const menu = document.querySelector('.navigation')
 const btn = document.querySelector('.hamburger')
 
